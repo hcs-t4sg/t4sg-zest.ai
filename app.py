4d52a6774765014d15b119abdb07160ad064f17d
Chris <christopherong@college.harvard.edu>
2021-02-23 20:40:25 -0800

    Updated README.md to include docker-compose commands. Other minor clean up

--- app.py
+++ app.py
@@ -4,10 +4,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
-
 @app.route('/hello')
 def say_hello_world():
     return {'result': "Flask says Hello World"}
