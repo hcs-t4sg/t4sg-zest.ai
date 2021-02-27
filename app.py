7e0cc150b453da1e77aa919b754bf9a08769957c
Chris <christopherong@college.harvard.edu>
2021-02-27 13:07:35 -0800

    Built out flask infrastructure for surgeo and zrp apis. Relevant to Kayla and Rakesh's tasks for this week

--- app.py
+++ app.py
@@ -7,3 +7,23 @@ CORS(app)
 @app.route('/hello')
 def say_hello_world():
     return {'result': "Flask says Hello World"}
+
+@app.route('/surgeo', methods=["GET"])
+def surgeo():
+    # TO-DO: @KAYLA
+    # Build out a public facing API that responds to a get request with the surname and zip code as query arguments. 
+    # The API should return the probabilities of each race prediction as a JSON object.
+    # Example of return object: {'AAPI': .56, 'Hispanic': .32, ..., 'White': .10}
+    pass
+
+@app.route('/zrp', methods=["GET"])
+def zrp():
+    # TO-DO: @RAKESH
+    # Build out a public facing API that responds to a get request with the full name, address, age, and gender as a query arguments. 
+    # The API should return the probabilities of each race prediction as a JSON object.
+    # Example of return object: {'AAPI': .56, 'Hispanic': .32, ..., 'White': .10}
+
+    # As part of this, you should add any relevant pickle files that you might want to call to the directory "picklefiles".
+    # These files will be stored on the server and your API will be able to call them in order to make predictions.
+    # Currently, there's just one file in the picklefiles folder: the pickled Florida predictor that was sent over by Kasey, but feel free to add more.
+    pass
