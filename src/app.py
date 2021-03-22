10f7f7fc67a87436bff75716d23933f8da2c4060
rakesh-nori <20rakeshn@students.harker.org>
2021-03-22 11:18:54 -0400

    Finished ZRP API

--- src/app.py
+++ src/app.py
@@ -75,10 +75,8 @@ def zrp():
     birth_date = request.args.get('birth_date')
 
     data = {'Name_First': [str(first_name)], 'Name_Last': [str(last_name)], 'Name_Middle': [str(middle_name)], 'Zipcode': [int(zipcode)], 'Precinct_Split': [str(precinct_split)], 'Gender': [str(gender)], 'County_Code': [str(county_code)], 'Congressional_District':[float(congressional_district)], 'Senate_District': [float(senate_district)], 'House_District': [float(house_district)], 'Birth_Date': [str(birth_date)]}
-    #data = {'Name_First': ['George'], 'Name_Last': ['Chambers'], 'Name_Middle': ['William'], 'Zipcode': [34293], 'Precinct_Split': ['NaN'], 'Gender': ['M'], 'County_Code': ['SAR'], 'Congressional_District':[17.0], 'Senate_District': [23.0], 'House_District': [74.0], 'Birth_Date': ['12/02/1951']}
     sample = pd.DataFrame(data)
     preds = zrp_predict.generatePredictions(sample)
-    #return str(preds[0][0])
 
 
     preds_data = {'American Indian': str(preds[0][0]), 'Asian Pacific Islander': str(preds[0][1]), 'Black': str(preds[0][2]), 'Hispanic': str(preds[0][3]), 'White': str(preds[0][4]), 'Other': str(preds[0][5]), 'Multi': str(preds[0][6])}
