1bad7316719d32a6cc4348fdceae4a65e108b106
rakesh-nori <20rakeshn@students.harker.org>
2021-03-25 01:55:31 -0400

    addressed Chris's comments

--- src/app.py
+++ src/app.py
@@ -74,12 +74,30 @@ def zrp():
     house_district = request.args.get('house_district')
     birth_date = request.args.get('birth_date')
 
-    data = {'Name_First': [str(first_name)], 'Name_Last': [str(last_name)], 'Name_Middle': [str(middle_name)], 'Zipcode': [int(zipcode)], 'Precinct_Split': [str(precinct_split)], 'Gender': [str(gender)], 'County_Code': [str(county_code)], 'Congressional_District':[float(congressional_district)], 'Senate_District': [float(senate_district)], 'House_District': [float(house_district)], 'Birth_Date': [str(birth_date)]}
+    data = {'Name_First': [str(first_name)], 
+            'Name_Last': [str(last_name)],
+            'Name_Middle': [str(middle_name)], 
+            'Zipcode': [int(zipcode)], 
+            'Precinct_Split': [str(precinct_split)], 
+            'Gender': [str(gender)], 
+            'County_Code': [str(county_code)], 
+            'Congressional_District':[float(congressional_district)], 
+            'Senate_District': [float(senate_district)], 
+            'House_District': [float(house_district)], 
+            'Birth_Date': [str(birth_date)]}
+    
     sample = pd.DataFrame(data)
     preds = zrp_predict.generatePredictions(sample)
 
 
-    preds_data = {'American Indian': str(preds[0][0]), 'Asian Pacific Islander': str(preds[0][1]), 'Black': str(preds[0][2]), 'Hispanic': str(preds[0][3]), 'White': str(preds[0][4]), 'Other': str(preds[0][5]), 'Multi': str(preds[0][6])}
+    preds_data = {'American Indian': str(preds[0][0]), 
+                  'Asian Pacific Islander': str(preds[0][1]), 
+                  'Black': str(preds[0][2]), 
+                  'Hispanic': str(preds[0][3]), 
+                  'White': str(preds[0][4]), 
+                  'Other': str(preds[0][5]), 
+                  'Multi': str(preds[0][6])}
+
     json_preds = json.dumps(preds_data)
     return json_preds
     
--- src/zrp_feature_engineering.py
+++ src/zrp_feature_engineering.py
@@ -11,19 +11,19 @@ from category_encoders import TargetEncoder
 
 class ZRPFeatureEngineering():
     '''This class is used to execute general ZRP feature engineering.''' 
-    
+
     def __init__(self):
-        
+
         # label encode via target
         self.label_encoded_columns = ['Name_First', 'Name_Last', 'Name_Middle', 'Zipcode', 'Precinct_Split']
-  
+
         # ordinal encoding
         self.ordinal_encoded_columns = ['Gender', 'County_Code']
         self.oe = OrdinalEncoder()
 
         # numerical columns (categories, but let the tree figure it out...)
         self.numerical_columns =  ['Congressional_District', 'Senate_District', 'House_District']
-        
+
         # dates
         self.date_columns = ['Birth_Date']
 
@@ -38,7 +38,7 @@ class ZRPFeatureEngineering():
         # handle multi-labeled output
         self.mlb = MultiLabelBinarizer(classes = y_unique)
         self.mlb_columns = [census_code_mapping[x] for x in y_unique]
-        
+
         self.mlb.fit(y.values.reshape(-1,1))
         y_ohe = pd.DataFrame(self.mlb.transform(y.values.reshape(-1,1)), columns=self.mlb_columns)
 
@@ -47,35 +47,35 @@ class ZRPFeatureEngineering():
             self.le[i] = TargetEncoder()
 
         return y_ohe
-    
+
     def fit(self, X, y):
-        
+
         X = X.reset_index(drop=True)
         y = y.reset_index(drop=True)        
-        
+
         y_ohe = self._process_target(y)
-        
+
         # fit label encoded columns
         for i in range(self.n_classes):
             self.le[i].fit(X[self.label_encoded_columns], y_ohe.iloc[:,i])
 
         # fit ordinal columns
         self.oe.fit(X[self.ordinal_encoded_columns]) 
-        
+
 
         return self
-    
+
     def transform(self, X):
 
         X = X.reset_index(drop=True)
-        
+
         # handle missing gender
         X[['Gender']] = X[['Gender']].replace(to_replace='nan', value='U')
-        
+
         # transform X
         X_date_convert = X[self.date_columns].apply(pd.to_datetime, errors='coerce')
         X_date_convert = X_date_convert[self.date_columns].apply(lambda x: getattr(pd.DatetimeIndex(x),'year'))
-  
+
         X_fe = pd.concat([self.le[i].transform(X[self.label_encoded_columns]) for i in range(self.n_classes)],
                          axis=1, sort=False
                         )
@@ -85,7 +85,7 @@ class ZRPFeatureEngineering():
                           X_date_convert[self.date_columns],
                           X[self.numerical_columns]
                          ], axis=1, sort=False)
-    
+
         label_encoded_colname = []
         for label in self.mlb_columns:
             for col in self.label_encoded_columns:
@@ -93,4 +93,4 @@ class ZRPFeatureEngineering():
 
         X_fe.columns = label_encoded_colname + self.ordinal_encoded_columns + self.date_columns + self.numerical_columns
 
-        return X_fe
+        return X_fe 
